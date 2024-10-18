b3ccc51e22bbd636de6d8cb2787a2b402db3e7fb
Stefano Marrone <stefano.marrone@unicampania.it>
2024-10-19 00:02:14 +0200

    recursive bottom up visitor for the grammar

---
+++ src/pdft/visitors.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+
+from lark import Token
+from lark.visitors import Visitor_Recursive
+
+
+class PdftVisitor(Visitor_Recursive):
+
+    def __init__(self):
+        self.functions = {
+            'dynamic_name_def': self.collect
+        }
+        self.dynamics = list()
+
+    def pprint(self):
+        print(f'Dynamics -> {self.dynamics}')
+
+
+    def _call_userfunc(self, tree):
+        payload: Token = tree.data
+        kind = payload.value
+        if kind in self.functions.keys():
+            function = self.functions[kind]
+            child = tree.children[0]
+            function(child.value)
+        return None
+
+    def collect(self, value):
+        self.dynamics.append(value)
+
