3f08db54399eaba457e8c518b5be5316be9f1e9f
Stefano Marrone <stefano.marrone@unicampania.it>
2024-10-20 14:46:45 +0200

    Opening to command line arguments

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
+import sys
+
 from src.pdft.errors import PdftError
 from src.pdft.parsers import PdftParser
 from src.pdft.validators import PdftValidator
-from src.pdft.visitors import PdftVisitor
 from src.utils import get_content
 
-grammar_filename = 'grammars/pdft.lark'
-
-examples = {
-    #'dynamics': 'examples/dynamics.pdftml'
-    #'component': 'examples/components.pdftml',
-    'full': 'examples/full.pdftml'
-}
-
 if __name__ == '__main__':
-    grammar: str = get_content(grammar_filename)
-    parser: PdftParser = PdftParser(grammar)
-    for example_name in examples.keys():
-        example = get_content(examples[example_name])
+    if len(sys.argv) > 2:
+        grammar_filename = sys.argv[1]
+        model_filename = sys.argv[2]
+        grammar: str = get_content(grammar_filename)
+        parser: PdftParser = PdftParser(grammar)
+        example = get_content(model_filename)
         tree = parser.parse(example)
         validator: PdftValidator = PdftValidator()
         validator.visit(tree)
